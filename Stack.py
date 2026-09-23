c20c1b6528a9854d73cd236c83efd52ea4c8a21d
조현규 <suo41874956@gmail.com>
2026-09-23 15:29:02 +0900

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -8,14 +8,12 @@ possible = True
 
 for i in range(n):
     target = int(input())
-
-    # target까지 스택에 넣기
+    
     while num <= target:
         stack.append(num)
         result.append("+")
         num += 1
 
-    # 스택의 가장 위 숫자가 target인지 확인
     if stack[-1] == target:
         stack.pop()
         result.append("-")
